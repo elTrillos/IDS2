93a9340e8e9bed82fcd9404ea6cea5f4f4a56467
benjamin gonzalez <bigonzalez@miuandes.cl>
2022-10-06 17:43:14 -0300

    vista del login

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ class User(db.Model):
 
 @app.route('/', methods=['POST','GET'])
 def index():
-        return render_template('index.html')
+        return render_template('login.html')
 
 @app.route("/login",methods=["GET", "POST"])
 def login():
